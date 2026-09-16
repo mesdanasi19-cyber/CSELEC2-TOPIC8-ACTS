7bf951106049c69a95690667c5043c58019c921b
mesdanasi19-cyber <mesdanasi19@gmail.com>
2026-09-16 09:00:39 +0800

    Added coins image

---
+++ CSELEC2 ACT#7/Circle-Detection.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('coins.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 5)
+
+circles = cv2.HoughCircles(
+    gray, cv2.HOUGH_GRADIENT, 1, 50, param1=25, param2=12
+)
+circles = np.uint16(np.around(circles))
+
+for c in circles[0, :]:
+  cv2.circle(img, (c[0], c[1]), c[2], (0, 255, 0), 2)
+
+cv2.imshow('Detected Circles', img)
+cv2.waitKey(0)
