3ebb52be896c672aa48aa88bd4f0f95ede2346ae
mesdanasi19-cyber <mesdanasi19@gmail.com>
2026-09-16 08:50:40 +0800

    Detect lines in NoGirl Image

--- CSELEC2 ACT#6/LineDetection.py
+++ CSELEC2 ACT#6/LineDetection.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('road.jpg')
+img = cv2.imread('NoGirl.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 50, 150)
 lines = cv2.HoughLinesP(
